935c3fdb3ea5e3b97309a87611ef57a2ac9f8e27
Edwin Islas Aguirre <8241001@isd535.org>
2022-04-21 11:02:35 -0500

    adding truck.js file

--- scripts/datastore.js
+++ scripts/datastore.js
@@ -12,7 +12,17 @@
         this.data[key] = val;
     };
 
-    DataStore.prototype.get = 
+    DataStore.prototype.get = function (key) {
+        return this.data[key];
+    };
+
+    DataStore.prototype.getAll = function () {
+        return this.data;
+    };
+
+    DataStore.prototype.remove = function(key) {
+        delete this.data[key]
+    }
 
     App.DataStore = DataStore;
     window.App = App;
---
+++ scripts/truck.js
@@ -0,0 +1,12 @@
+(function (window) {
+    'use strict';
+
+    let App = window.App || {};
+
+    function Truck() {
+    }
+
+    App.Truck = Truck;
+    window.App = App;
+
+})(window);
