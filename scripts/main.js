f989c661c2a0f1d2131afa0273e4ac70b1bfecd5
Edwin Islas Aguirre <8241001@isd535.org>
2022-04-22 11:21:35 -0500

    made changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,12 @@
-// Put your JavaScript here
+(function (window) {
+    'use strict';
+
+    let App = window.App;
+    let Truck = App.Truck;
+    let DataStore = App.DataStore;
+
+    let myTruck = new Truck('12345', new DataStore());
+
+    window.myTruck = myTruck;
+
+})(window);
