4b6aed5b3969cc9a625e1f4850deccb55f958b29
Edwin Islas Aguirre <8241001@isd535.org>
2022-04-28 11:24:51 -0500

    added a lot of new things

--- scripts/checklist.js
+++ scripts/checklist.js
@@ -13,22 +13,52 @@
         }
     }
 
+    // The method that adds a new row to the checklist
+    CheckList.prototype.addRow = function (volleyballOrder) {
+        // Remove any existing rows that match the email Address
+        this.removeRow(volleyballOrder, emailAddress);
+        // Create a new instance of row, using volleyball order info
+        var rowElement = new Row(volleyballOrder);
+        // Add the new row instance's $element property to the checklist
+        this.$element.append(rowElement.$element);
+    };
+
+    // remove a row identified by an email address
+    CheckList.prototype.removeRow = function (email) {
+        this.$element
+        .find('[value="' + email + '"]')
+        .remove();
+    };
+
     // each row is an outstanding order
     function Row(volleyballOrder) {
-            let $div = $('<div><div>', {
-                'data-volleyball-order': 'checkbox',
-                'class': 'checkbox'
-            });
-            let $label = $('<label><label>');
+        let $div = $('<div><div>', {
+            'data-volleyball-order': 'checkbox',
+            'class': 'checkbox'
+        });
+        let $label = $('<label><label>');
 
-            let $checkbox = $('<label><label>', {
-                type: 'checkbox',
-                value: volleyballOrder.emailAddress
-            }
+        let $checkbox = $('<input><input>', {
+            type: 'checkbox',
+            value: volleyballOrder.emailAddress
         });
 
+        let description = volleyballOrder.size + ' ';
+        if (volleyballOrder.flavor) {
+            description += volleyballOrder.flavor + ' ';
+        }
+        description += volleyballOrder.item + ' ';
+        description += '(' + volleyballOrder.emailAddress + ')';
+        description += '[' + volleyballOrder.strength + 'x]';
+
+        $label.append($checkbox);
+        $label.append(description);
+        $label.append($label);
+
+        this.$element = $div;
+        }
 
         // Add the Checklist to the namespace
         App.CheckList = CheckList;
         window.App = App;
-})(window);
+ })(window);
--- scripts/main.js
+++ scripts/main.js
@@ -3,19 +3,23 @@
 
     const FORM_SELECTOR ='[data-volleyball-order="form"]';
 
-
     // lets make sure we only have one of these
     let App = window.App;
     let Truck = App.Truck;
     let DataStore = App.DataStore;
     let FormHandler = App.FormHandler;
+    let CheckList = App.CheckList;
 
     let myTruck = new Truck('12345', new DataStore());
 
     window.myTruck = myTruck;
 
     let formHandler = new FormHandler(FORM_SELECTOR);
-    formHandler.addSubmitHandler();
-    console.log(formHandler);
+   
+    // when the submit button is called, create the order and add a checkbox
+    formHandler.addSubmitHandler(function (data) {
+        myTruck.createOrder.call(myTruck, data);
+        checkList.addRow.call(checkList, data);
+    });
 
 })(window);
