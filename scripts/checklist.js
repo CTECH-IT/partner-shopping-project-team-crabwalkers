1c2655039d59479c0a18e2e9fa2f77c0bd7beffe
Edwin Islas Aguirre <8241001@isd535.org>
2022-04-29 10:41:21 -0500

    changes made

--- scripts/checklist.js
+++ scripts/checklist.js
@@ -15,68 +15,46 @@
 
     // The method that adds a new row to the checklist
     CheckList.prototype.addRow = function (volleyballOrder) {
-
-        // Remove any existing rows that match the email Address
-        this.removeRow(volleyballOrder, emailAddress);
-        // Create a new instance of row, using volleyball order info
-        var rowElement = new Row(volleyballOrder);
-        // Add the new row instance's $element property to the checklist
-        this.$element.append(rowElement.$element);
-    };
-
-    // remove a row identified by an email address
-    CheckList.prototype.removeRow = function (email) {
-        this.$element
-        .find('[value="' + email + '"]')
-        .remove();
-    };
-
-    // each row is an outstanding order
-    function Row(volleyballOrder) {
-        let $div = $('<div><div>', {
-            'data-volleyball-order': 'checkbox',
-            'class': 'checkbox'
-        });
-        let $label = $('<label><label>');
-
-        let $checkbox = $('<input><input>', {
-
         // Create a new instance of a row, using the coffee order info
         var rowElement = new Row(volleyballOrder);
+        // Remove any existing rows that match the email Address
+        this.removeRow(volleyballOrder, emailAddress);
         // Add the new row instance's $element prototype to the checklist
         this.$element.append(rowElement.$element);
-    });
+    };
 
     // Each row is one Outstanding Order
     function Row(volleyballOrder) {
         let $div = $('<div></div>', {
-            'data-volleyball-order': 'checkbox',
+            'data-coffee-order': 'checkbox',
             'class': 'checkbox'
         });
         let $label = $('<label></label>');
 
         let $checkbox = $('<input></input>', {
-
             type: 'checkbox',
             value: volleyballOrder.emailAddress
         });
 
         let description = volleyballOrder.size + ' ';
+
         if (volleyballOrder.flavor) {
             description += volleyballOrder.flavor + ' ';
         }
-        description += volleyballOrder.item + ' ';
-        description += '(' + volleyballOrder.emailAddress + ')';
-        description += '[' + volleyballOrder.strength + 'x]';
+        description += volleyballOrder.coffee + ', ';
+        description += ' (' + volleyballOrder.emailAddress + ')';
+        description += ' [' + volleyballOrder.strength + 'x]';
 
         $label.append($checkbox);
         $label.append(description);
-        $label.append($label);
+        $div.append($label);
 
         this.$element = $div;
-        }
 
-        // Add the Checklist to the namespace
-        App.CheckList = CheckList;
-        window.App = App;
- })(window);
+    }
+
+    // Add the Checklist to the App namespace
+    App.CheckList = CheckList;
+    window.App = App;
+
+})(window);
