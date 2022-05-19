23c20673c472dfd0ce4d0cfc4919a72c0923484d
Edwin Islas Aguirre <8241001@isd535.org>
2022-05-19 11:01:37 -0500

    still working, some confusion

--- scripts/checklist.js
+++ scripts/checklist.js
@@ -50,26 +50,12 @@
             value: volleyballOrder.emailAddress
         });
 
-<<<<<<< HEAD
         let description = volleyballOrder.item + ' ';
         if (volleyballOrder.item) {
             description+= volleyballOrder.item + ' ';
         }
         description += volleyballOrder.equipment + ', ';
         description += ' (' + volleyballOrder.emailAddress + ')';
-=======
-        let description = volleyballOrder.size + ' ';
-        if (volleyballOrder.flavor) {
-            description += volleyballOrder.flavor + ' ';
-        }
-        description += volleyballOrder.coffee + ', ';
-        description += ' (' + volleyballOrder.emailAddress + ')';
-        description += ' [' + volleyballOrder.strength + 'x]';
-  
-        $label.append($checkbox);
-        $label.append(description);
-        $div.append($label);
->>>>>>> 83efb3d84b6bedd99272cd4da0e7773676fd2ec5
 
         $label.append($checkbox);
         $label.append(description);
